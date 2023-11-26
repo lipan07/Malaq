d18b7335e453987eb51142d64c0c5a80da9cb08a
lipan07 <lipan.biswas.dev@gmail.com>
2023-11-26 12:30:34 +0530

    changes

--- App.js
+++ App.js
@@ -9,44 +9,13 @@ import {
   Alert,
   Pressable,
 } from "react-native";
+import GoalInput from './components/GoalInput';
 
 export default function App() {
-  const [inputText, setInputText] = useState("");
-  const [courseGoals, setCourseGoals] = useState([]);
-  const inputTextHandler = (inputText) => {
-    setInputText(inputText);
-  };
-  const handlePress = () => {
-    // console.log('Do not click this button again!!');
-    // Alert.alert('Do not click this button again!!');
-    setCourseGoals((currentCourseGoals) => [...currentCourseGoals, inputText]);
-    console.log(courseGoals);
-  };
   return (
     <View style={styles.allContainer}>
-      <View style={styles.searchContainer}>
-        <TextInput
-          style={styles.input}
-          placeholder="Type here.."
-          onChangeText={inputTextHandler}
-        />
-        <Pressable
-          style={styles.btn}
-          title="Click here.."
-          onPress={handlePress}
-        >
-          <Text>Click here..</Text>
-        </Pressable>
-      </View>
-      <View>
-        {courseGoals.map((goal, i) => (
-          <View key={i} style={styles.goalView}>
-            <Text style={styles.goal}>
-              {i + 1}. {goal}
-            </Text>
-          </View>
-        ))}
-      </View>
+      
+      <GoalInput />
 
       <View style={styles.container}>
         <View style={styles.container}>
@@ -89,38 +58,5 @@ const styles = StyleSheet.create({
     flexDirection: "column",
     flex: 1,
     marginTop: 50,
-  },
-  searchContainer: {
-    flexDirection: "row",
-    // flex: 1
-  },
-  container: {
-    flexDirection: "row",
-    flex: 1,
-  },
-  input: {
-    borderWidth: 1,
-    borderColor: "red",
-    height: 40,
-    flex: 1,
-    margin: 5,
-    padding: 5,
-  },
-  btn: {
-    margin: 5,
-    padding: 5,
-    margin: 5,
-  },
-  goal: {
-    margin: 2,
-    padding: 10,
-    fontWeight: "bold",
-    color: "#ffff",
-  },
-  goalView: {
-    borderWidth: 1,
-    borderColor: "blue",
-    backgroundColor: "green",
-    marginBottom: 2,
-  },
+  }
 });
